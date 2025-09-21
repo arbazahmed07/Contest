d8a019a7ac4591643230fa581f4b17f761704a13
arbazahmed07 <mdarbazking7@gmail.com>
2025-09-21 13:46:17 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -24,6 +24,8 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5000",
+      "https://contest-7tme.onrender.com",
+      "https://contest-delta.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
